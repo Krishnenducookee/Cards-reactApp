53e532c8239432a64c2e161e6a0798d2c9b39965
krishnendukrish <krishnendhuk165836@gmail.com>
2023-08-22 16:20:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Card_Component from "./Catr_Component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="bg-white h-screen pt-40 px-80">
+        <label class="relative inline-flex items-center cursor-pointer">
+          <input type="checkbox" value="" class="sr-only peer" />
+          <div
+            class="w-11 h-6 bg-black outline-white peer-focus:ring-4
+           peer-focus:ring-white dark:peer-focus:ring-white rounded-full 
+           peer dark:bg-black peer-checked:after:translate-x-full
+            peer-checked:after:border-black after:content-[''] after:absolute 
+            after:top-[2px] after:left-[2px] after:bg-black after:border-white 
+            after:border after:rounded-full after:h-5 after:w-5 after:transition-all
+             dark:border-black peer-checked:bg-white"
+          ></div>
+          <span class="ml-3 text-sm font-medium text-black">Change Theme</span>
+        </label>
+        <div>
+          <Card_Component />
+        </div>
+        <div className="ml-96 -mt-96 ">
+          <Card_Component />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Catr_Component.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Card_Component = () => {
+  return (
+    <div className='rounded bg-black w-80 h-96'>
+        <div className="px-6 py-4">
+            <p className="text-white">
+              Is simply dummy text of the printing and typesetting industry.
+              Lorem Ipsum has been the industry's standard dummy text ever since
+              the 1500s, when an unknown printer took a galley of type and
+              scrambled it to make a type specimen book. It has survived not
+              only five centuries, but also the leap into electronic
+              typesetting, remaining essentially unchanged. It was popularised
+              in the 1960s with the release of Letraset sheets containing Lorem
+              Ipsum passages.
+            </p>
+          </div></div>
+  )
+}
+
+export default Card_Component
