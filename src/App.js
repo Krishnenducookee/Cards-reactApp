952b68fb1ddd3265f900812174eefbdf8313ba4d
Krishnenducookee <141391502+Krishnenducookee@users.noreply.github.com>
2023-08-23 12:24:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,18 +5,13 @@ export const themeContext = createContext(null);
 
 function App() {
   const [theme, changeTheme] = useState("white");
-  // const [theme, changeTheme] = useState({ background: "white", text: "black" });
 
   const textColor = theme === "white" ? "black" : "white";
-
-  const backgroundClassName = "h-screen pt-40 px-80 bg-" + theme;
-
-  const textClassName = "ml-3 text-sm font-medium text-" + textColor;
 
   return (
     <div>
       <themeContext.Provider value={theme}>
-        <div className={backgroundClassName}>
+        <div className={`h-screen pt-40 px-80 bg-${theme}`}>
           <label className="relative inline-flex items-center cursor-pointer">
             <input
               type="checkbox"
@@ -24,23 +19,19 @@ function App() {
               className="sr-only peer"
               onClick={() => {
                 changeTheme(theme === "white" ? "black" : "white");
-                // preState.background === "white"
-                //   ? ((preState.background = "black"),
-                //     (preState.text = "white"))
-                //   : ((preState.background = "white"),
-                //     (preState.text = "black"))
               }}
             />
             <div
-              className="w-11 h-6 bg-black outline-white peer-focus:ring-4
-           peer-focus:ring-white dark:peer-focus:ring-white rounded-full 
+              className="w-11 h-6 bg-black  rounded-full 
            peer dark:bg-black peer-checked:after:translate-x-full
             peer-checked:after:border-black after:content-[''] after:absolute 
             after:top-[2px] after:left-[2px] after:bg-black after:border-white 
             after:border after:rounded-full after:h-5 after:w-5 after:transition-all
              dark:border-black peer-checked:bg-white"
             ></div>
-            <span className={textClassName}>Change Theme</span>
+            <span className={`ml-3 text-sm font-medium text-${textColor}`}>
+              Change Theme
+            </span>
           </label>
           <div>
             <Card_Component />
