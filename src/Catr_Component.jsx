f0014e4fdb1be2a6d100c7f8179290a12c93039f
Krishnenducookee <141391502+Krishnenducookee@users.noreply.github.com>
2023-08-23 13:19:23 +0530

    Update Card_Component.jsx

--- src/Catr_Component.jsx
+++ src/Catr_Component.jsx
@@ -1,12 +1,12 @@
-
-import { themeContext } from './App'
+import { useContext } from "react"
+import { themeContext } from "./App"
 
 const Card_Component = () => {
-  
+  const contextValue=useContext(themeContext)
   return (
-     <themeContext.Consumer>{contextValue=>{
+    //  <themeContext.Consumer>{contextValue=>{
 
-     return (
+    //  return (
     <div className={`rounded w-80 h-96 bg-${contextValue==="white"? "black" :"white"}`}>
         <div className="px-6 py-4">
             <p className={`text-${contextValue}`}>
@@ -14,8 +14,9 @@ const Card_Component = () => {
               This is Text inside Cards<br/>
               This is Text inside Cards<br/>
             </p>
-          </div></div>)}}
-           </themeContext.Consumer>
+          </div></div>
+          // )}}
+          //  </themeContext.Consumer>
   )
 }
 
