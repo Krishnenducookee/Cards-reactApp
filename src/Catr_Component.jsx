c797ed4318a2a0f978751d8c6c7b361888615f42
Krishnenducookee <141391502+Krishnenducookee@users.noreply.github.com>
2023-08-23 12:23:35 +0530

    Update Card_Component.jsx

--- src/Catr_Component.jsx
+++ src/Catr_Component.jsx
@@ -1,3 +1,4 @@
+
 import { themeContext } from './App'
 
 const Card_Component = () => {
@@ -5,16 +6,12 @@ const Card_Component = () => {
   return (
      <themeContext.Consumer>{contextValue=>{
 
-      const cardBackground=contextValue==="white"?"black":"white";
-  
-      
-      const cardClassName="rounded w-80 h-96 bg-"+cardBackground;
-      const textClassName="text-"+contextValue
+      const cardBackground = contextValue==="white"? "black" :"white";
 
      return (
-    <div className={cardClassName}>
+    <div className={`rounded w-80 h-96 bg-${cardBackground}`}>
         <div className="px-6 py-4">
-            <p className={textClassName}>
+            <p className={`text-${contextValue}`}>
               This is Text inside Cards<br/>
               This is Text inside Cards<br/>
               This is Text inside Cards<br/>
