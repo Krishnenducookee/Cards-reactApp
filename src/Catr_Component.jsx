a522461aee146348b0108a864faa92789b2ea993
Krishnenducookee <141391502+Krishnenducookee@users.noreply.github.com>
2023-08-24 11:21:20 +0530

    Update Catr_Component.jsx

--- src/Catr_Component.jsx
+++ src/Catr_Component.jsx
@@ -1,19 +1,26 @@
-import { useContext } from "react"
+import { useContext,useState  } from "react"
 import { themeContext } from "./App"
 
+
 const Card_Component = () => {
-  const contextValue=useContext(themeContext)
+   const contextValue =useContext(themeContext)
+   const [text, settext] = useState("")
+   
+    const cardBackground=contextValue?"bg-black":"bg-white"
+    const textColor=contextValue?"white":"black"
   return (
     //  <themeContext.Consumer>{contextValue=>{
-
+  
     //  return (
-    <div className={`rounded w-80 h-96 bg-${contextValue==="white"? "black" :"white"}`}>
+    <div className={`rounded w-80 h-96 ${cardBackground}`}>
         <div className="px-6 py-4">
-            <p className={`text-${contextValue}`}>
+            <p className={`text-${textColor}`}>
               This is Text inside Cards<br/>
               This is Text inside Cards<br/>
               This is Text inside Cards<br/>
             </p>
+            <input type="text" name="text1" className="mt-6"
+             placeholder="Type here" onChange={(e)=>{settext(e.target.value)}}/>
           </div></div>
           // )}}
           //  </themeContext.Consumer>
