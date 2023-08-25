92e1717cc5918b97ef1542cec1f0375656eabb3b
Krishnenducookee <141391502+Krishnenducookee@users.noreply.github.com>
2023-08-25 12:00:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,44 +1,63 @@
-import { createContext, useState } from "react";
-import Card_Component from "./Catr_Component";
+import { createContext, useEffect, useState } from "react";
+import CardComponent from "./Catr_Component";
 
-export const themeContext = createContext(null);
+export const ThemeContext = createContext(null);
 
 function App() {
-  const [theme, changeTheme] = useState(true);
-  const background=theme?"white":"black"
-  const textColor=theme?"black":"white"
-  
+
+  const [isWhite, toggleWhite] = useState(true);
+  const [sessionStorageData, setSessionStorageData] = 
+              useState({textFromLeftField:"",textFromRightField:""})
+
+  const background=isWhite?"white":"black"
+  const textColor=isWhite?"black":"white"
+   
+  useEffect(()=>{ 
+        setSessionStorageData(
+          {textFromLeftField:sessionStorage.getItem('textFromLeftField'),
+          textFromRightField:sessionStorage.getItem('textFromRightField')}
+          )
+   },[])
+
+
+  const components=[
+           {text:sessionStorageData.textFromLeftField,
+            onChange:(texttyped)=>{sessionStorage.setItem('textFromLeftField',texttyped)}},
+           {text:sessionStorageData.textFromRightField,
+            onChange:(texttyped)=>{sessionStorage.setItem('textFromRightField',texttyped)}}]  
+
   return (
     <div>
-      <themeContext.Provider value={theme}>
+      <ThemeContext.Provider value={isWhite}>
         <div className={`h-screen pt-40 px-80 bg-${background}`}>
           <label className="relative inline-flex cursor-pointer">
             <input
               type="checkbox"
               className="sr-only peer"
               onClick={() => {
-               changeTheme(theme?false:true);}}
+                toggleWhite(!isWhite);}}
             />
             <div
               className="w-11 h-6 bg-black rounded-full after:absolute
-            after:rounded-full after:h-5 after:w-5  after:border after:border-white
+            after:rounded-full after:h-4 after:w-5  after:border after:border-white
             after:bg-black after:mt-1 after:transition-all peer-checked:bg-white
-            peer-checked:after:translate-x-full"
+            peer-checked:after:translate-x-full after:ml-1"
             ></div>
             <span
               className={`ml-3 text-sm font-medium text-${textColor}`}
             >
               Change Theme
             </span>
-          </label>
-          <div>
-            <Card_Component />
-          </div>
-          <div className="ml-96 -mt-96 ">
-            <Card_Component />
-          </div>
+          </label>     
+          {components.map((data,index)=>(
+           <div className={index===1?"ml-96 -mt-96":""}>
+           <CardComponent text={data.text} 
+           onChange={data.onChange}/>
+           </div>
+             ))} 
+
         </div>
-      </themeContext.Provider>
+      </ThemeContext.Provider>
     </div>
   );
 }
